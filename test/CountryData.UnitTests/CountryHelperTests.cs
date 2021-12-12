45bb8c335093b1e36e672af78894ef2cb425f747
jkaddo91 <jkaddo91@gmail.com>
2021-12-12 16:10:27 +0000

    Write test for GetCountry

--- test/CountryData.UnitTests/CountryHelperTests.cs
+++ test/CountryData.UnitTests/CountryHelperTests.cs
@@ -1,9 +1,24 @@
+using CountryData.Standard;
+using FluentAssertions;
+using System.Collections.Generic;
 using Xunit;
 
-namespace CountryData.UnitTests
+namespace CountryData.UnitTests;
+
+public class CountryHelperTests
 {
-    public class CountryHelperTests
+    //Arrange
+    private readonly CountryHelper _countryHelper  = new();
+
+    [Fact]
+    public void GetCountryData_ShouldReturnListOfCountryData()
     {
-        
+        //Act
+        var countryData = _countryHelper.GetCountryData();
+
+        //Assert
+        countryData.Should().BeAssignableTo<IEnumerable<Country>>();
+        countryData.Should().NotBeNullOrEmpty();
     }
+
 }
