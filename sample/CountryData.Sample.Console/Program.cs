8285fba6e48504642560831e7c69e7375a520e4e
jkaddo91 <jkaddo91@gmail.com>
2021-12-13 14:49:24 +0000

    Add Sample Projects

---
+++ sample/CountryData.Sample.Console/Program.cs
@@ -0,0 +1,17 @@
+﻿using CountryData.Standard;
+
+var countryHelper = new CountryHelper();
+
+
+var countryData = countryHelper.GetCountryData().Take(10);
+foreach (var country in countryData)
+{
+    Console.WriteLine($"{country.CountryName} ({country.CountryShortCode}) has {country.Regions.Count} regions");
+}
+
+
+var countryByCode = countryHelper.GetCountry("GHA").ToList();
+foreach (var country in countryByCode)
+{
+    Console.WriteLine($"{country.CountryName} ({country.CountryShortCode}) has {country.Regions.Count} regions");
+}
